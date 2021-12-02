0cd20674c8dff9eb323eb50b05b772497e36b3e1
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2021-12-01 22:59:26 -0800

    day 2

---
+++ day2.py
@@ -0,0 +1,32 @@
+data = open("day2.txt", "r").read().strip()
+data = data.split("\n")
+
+horiz_pos = 0
+depth = 0
+
+for i in data:
+    direction, magnitude = i.split(" ")
+    if direction == "forward":
+        horiz_pos += int(magnitude)
+    elif direction == "down":
+        depth += int(magnitude)
+    elif direction == "up":
+        depth -= int(magnitude)
+
+print(horiz_pos * depth)
+
+horiz_pos = 0
+depth = 0
+aim = 0
+
+for i in data:
+    direction, magnitude = i.split(" ")
+    if direction == "forward":
+        horiz_pos += int(magnitude)
+        depth += aim * int(magnitude)
+    elif direction == "down":
+        aim += int(magnitude)
+    elif direction == "up":
+        aim -= int(magnitude)
+
+print(horiz_pos * depth)

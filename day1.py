02fb587eb212fe543acc0373ab5625198f72d7d9
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2021-12-01 22:59:11 -0800

    day 1

---
+++ day1.py
@@ -0,0 +1,22 @@
+data = open("day1.txt", "r").read().strip()
+data = list(map(int, data.split("\n")))
+
+p1 = 0
+
+for i in range(1, len(data)):
+    if data[i] > data[i - 1]:
+        p1 += 1
+
+print(p1)
+
+curr = sum(data[:2])
+p2 = 0
+
+for i in range(3, len(data)):
+    new = curr + data[i] - data[i - 3]
+    if new > curr:
+        p2 += 1
+
+    curr = new
+
+print(p2)

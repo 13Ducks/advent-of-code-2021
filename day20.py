65d730494832ad078315ee12aea650b3d1998c5c
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2021-12-20 15:45:15 +0530

    better padding

--- day20.py
+++ day20.py
@@ -7,12 +7,15 @@ get_algo_v = np.vectorize(lambda x: algo[x])
 inp = [list(line) for line in inp.splitlines()]
 
 a = np.array(inp)
-a = np.pad(a, 100)
 a[a == "#"] = 1
 a[a == "."] = 0
 a = a.astype(int)
 
 for i in range(50):
+    # won't work for test input since first char is ".", so empty point stays "."
+    # however for real input, first char is "#", so empty points change to "#"
+    # and then last char is "." so full points go back to "."
+    a = np.pad(a, 2, "constant", constant_values=(i % 2))
     out = np.zeros([a.shape[0] - 2, a.shape[1] - 2], dtype=int)
     out += 2 ** 8 * a[:-2, :-2]
     out += 2 ** 7 * a[:-2, 1:-1]
